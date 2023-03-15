d4ecb6f52ff67cfa4afd65b203dae803ed200881
MoinuddinZaheer <mueenuddinzaheer@gmail.com>
2023-03-15 13:30:36 +0530

    change

--- script.js
+++ script.js
@@ -3,20 +3,26 @@ let cityName=document.getElementById('cityName');
 let temp=document.getElementById('temp') //h1 tag of temprature
 // let inputValue = search.value;
 
-let getInfo = async(e) => {
+let getInfo = async() => {
     let inputValue = search.value;
     
-    if (search.value=="") {
+    if (inputValue=="") {
         //error message
         alert("please enter a city name");
     } else {
         //fetch the data from api
         let url=`http://api.openweathermap.org/data/2.5/weather?q=${inputValue}&units=metric&appid=1cb6532aea3c298a830a71380eace21e`
        let response =await fetch(url)
-       //console.log(response);
+     
       let data=await response.json();  //fetching only jason
-    //   console.log(data);
-    cityName.textContent= `${data.name} \ ${data.sys.country}`;
+    
+    if(data.name)
+    {
+        cityName.textContent= `${data.name} \ ${data.sys.country}`;
+    }else{
+        cityName.textContent="Not found / you entered wrong city name"
+    }
     temp.textContent=data.main.temp;
+    console.log(data);
     }
 }
